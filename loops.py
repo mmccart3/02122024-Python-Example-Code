4d7b0c4abe34264a26f018c5e3fd528c41127d32
Mark McCarthy <mm3guides@gmail.com>
2024-12-12 16:29:15 +0000

    challenges - loops

--- loops.py
+++ loops.py
@@ -20,10 +20,22 @@ my_favourite_films = [
 for film in my_favourite_films:
     film_check(film)
     
-for i in range(0,10):
-    print(i)
-    sleep(0.2)
+# for i in range(0,10):
+#     print(i)
+#     sleep(0.2)
     
-for i in range(9,-1,-1):
-    print(i)
-    sleep(0.2)    
+# for i in range(9,-1,-1):
+#     print(i)
+#     sleep(0.2)  
+    
+from random import randint
+
+rand_num = randint(1,50)
+target_number = 50
+
+while rand_num != target_number:
+    print(rand_num)
+    sleep(0.1)
+    rand_num= randint(1,50)
+
+print(f"You have hit the target number of {rand_num}")
